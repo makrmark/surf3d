daa475bd83686aa03654c5239db39b371fde3469
Mark Cowlishaw <mark.cowlishaw@gmail.com>
2025-03-28 18:34:39 +1100

    add main.js as entry point

--- build.js
+++ build.js
@@ -4,21 +4,25 @@ const { htmlPlugin } = require('@craftamap/esbuild-plugin-html');
 esbuild.build({
     outdir: 'dist',
     bundle: true,
-    metafile: true,
+    metafile: true, // Required by htmlPlugin
     plugins: [
         htmlPlugin({
             files: [
                 {
-                    filename: 'index.html',
-                    htmlTemplate: 'index.html',
+                    entryPoints: ['main.js'], // Explicitly list main.js as an entry point
+                    filename: 'index.html',   // Output HTML file name in dist
+                    htmlTemplate: 'index.html', // Source HTML file to process
                 },
             ],
         }),
     ],
-    entryNames: '[dir]/[name]-[hash]',
+    entryNames: '[dir]/[name]-[hash]', // Output files like main-abc123.js
     chunkNames: '[name]-[hash]',
     assetNames: '[name]-[hash]',
     minify: true, // Optional: reduces file size
 }).then(() => {
     console.log('Build completed');
-}).catch(() => process.exit(1));
+}).catch((error) => {
+    console.error('Build failed:', error);
+    process.exit(1);
+});
