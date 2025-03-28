ead590507ea8f2e3980335c09d40fb1d20dab089
Mark Cowlishaw <mark.cowlishaw@gmail.com>
2025-03-28 18:43:44 +1100

    added debugging

--- build.js
+++ build.js
@@ -4,24 +4,24 @@ const { htmlPlugin } = require('@craftamap/esbuild-plugin-html');
 esbuild.build({
     outdir: 'dist',
     bundle: true,
-    metafile: true, // Required by htmlPlugin
+    metafile: true,
     plugins: [
         htmlPlugin({
             files: [
                 {
-                    entryPoints: ['main.js'], // Explicitly list main.js as an entry point
-                    filename: 'index.html',   // Output HTML file name in dist
-                    htmlTemplate: 'index.html', // Source HTML file to process
+                    entryPoints: ['main.js'], // Ensure main.js is bundled
+                    filename: 'index.html',   // Output HTML file
+                    htmlTemplate: 'index.html', // Source HTML file
                 },
             ],
         }),
     ],
-    entryNames: '[dir]/[name]-[hash]', // Output files like main-abc123.js
+    entryNames: '[dir]/[name]-[hash]',
     chunkNames: '[name]-[hash]',
     assetNames: '[name]-[hash]',
-    minify: true, // Optional: reduces file size
-}).then(() => {
-    console.log('Build completed');
+    minify: true,
+}).then((result) => {
+    console.log('Build completed. Outputs:', Object.keys(result.metafile.outputs));
 }).catch((error) => {
     console.error('Build failed:', error);
     process.exit(1);
