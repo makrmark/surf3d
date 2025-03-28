3abd8719a5d92ac804553ce127a002ae72a3a29d
Mark Cowlishaw <mark.cowlishaw@gmail.com>
2025-03-28 23:07:20 +1100

    i think i've fixed it

--- build.js
+++ build.js
@@ -1,29 +1,24 @@
 const esbuild = require('esbuild');
-const { htmlPlugin } = require('@craftamap/esbuild-plugin-html');
+const fs = require('fs');
 
 esbuild.build({
-    entryPoints: ['main.js'], // Explicitly bundle main.js
+    entryPoints: ['main.js'],
     outdir: 'dist',
     bundle: true,
     format: 'esm',
     metafile: true,
     external: ['three'],
-    plugins: [
-        htmlPlugin({
-            files: [
-                {
-                    filename: 'index.html',
-                    htmlTemplate: 'index.html',
-                },
-            ],
-        }),
-    ],
     entryNames: '[dir]/[name]-[hash]',
     chunkNames: '[name]-[hash]',
     assetNames: '[name]-[hash]',
     minify: false,
 }).then((result) => {
+    const mainFile = Object.keys(result.metafile.outputs).find(f => f.includes('main'));
+    const html = fs.readFileSync('index.html', 'utf8')
+        .replace('<script type="module" src="main.js">', `<script type="module" src="${mainFile.replace('dist/', '')}">`);
+    fs.writeFileSync('dist/index.html', html);
     console.log('Build completed. Outputs:', Object.keys(result.metafile.outputs));
+    console.log('Generated index.html:', fs.readFileSync('dist/index.html', 'utf8'));
 }).catch((error) => {
     console.error('Build failed:', error);
     process.exit(1);
