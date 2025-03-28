3c75152752ed55d92d7a33c2d3675e44e61f103e
Mark Cowlishaw <mark.cowlishaw@gmail.com>
2025-03-28 22:39:10 +1100

    fix entry points

--- build.js
+++ build.js
@@ -2,20 +2,17 @@ const esbuild = require('esbuild');
 const { htmlPlugin } = require('@craftamap/esbuild-plugin-html');
 
 esbuild.build({
-    entryPoints: ['main.js'],
     outdir: 'dist',
     bundle: true,
-    format: 'esm', // Ensure ES module output
+    format: 'esm',
     metafile: true,
     external: ['three'],
     plugins: [
         htmlPlugin({
             files: [
                 {
-                    entryPoints: ['main.js'],
-                    filename: 'index.html',
-                    htmlTemplate: 'index.html',
-                    scriptType: 'module', // Enforce module type
+                    filename: 'index.html',      // Output file in dist
+                    htmlTemplate: 'index.html',  // Source file
                 },
             ],
         }),
@@ -23,10 +20,9 @@ esbuild.build({
     entryNames: '[dir]/[name]-[hash]',
     chunkNames: '[name]-[hash]',
     assetNames: '[name]-[hash]',
-    minify: false, // Keep off for now
+    minify: false,
 }).then((result) => {
     console.log('Build completed. Outputs:', Object.keys(result.metafile.outputs));
-    console.log('Main file content:', require('fs').readFileSync(Object.keys(result.metafile.outputs).find(f => f.includes('main')), 'utf8'));
 }).catch((error) => {
     console.error('Build failed:', error);
     process.exit(1);
